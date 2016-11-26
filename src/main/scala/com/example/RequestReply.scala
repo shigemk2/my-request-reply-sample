246fe03cb0059822ef81666590927c001234a452
shigemk2 <i.am.shige@gmail.com>
2016-11-27 00:40:33 +0900

    Add client

--- src/main/scala/com/example/RequestReply.scala
+++ src/main/scala/com/example/RequestReply.scala
@@ -11,6 +11,12 @@ case class StartWith(server: ActorRef)
 
 class Client extends Actor {
   def receive = {
+    case StartWith(server) =>
+      println("Client: is starting....")
+      server ! Request("REQ-1")
+    case Reply(what) =>
+      println("Client: received response: " + what)
+      RequestReplyDriver.completedStep()
     case _ =>
       println("Client: received unexpected message")
   }
