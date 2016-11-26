99f30436e6498cf5576e0294cf096e1bedf16f57
shigemk2 <i.am.shige@gmail.com>
2016-11-27 00:35:30 +0900

    Add case class

--- src/main/scala/com/example/RequestReply.scala
+++ src/main/scala/com/example/RequestReply.scala
@@ -5,6 +5,10 @@ import akka.actor._
 object RequestReplyDriver extends CompletableApp(1) {
 }
 
+case class Request(what: String)
+case class Reply(what: String)
+case class StartWith(server: ActorRef)
+
 class Client extends Actor {
   def receive = {
     case _ =>
